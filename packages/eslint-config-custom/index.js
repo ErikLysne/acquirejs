c2085852fe4e7c0abed1346d53c7965ed6b5146e
Erik Lysne <erik.lysne@inline-test.no>
2023-05-25 13:06:49 +0200

    Configured monorepo structure

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+  }
 };
