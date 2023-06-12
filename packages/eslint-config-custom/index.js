7f067766739c174ae9c2e1a4aac299a844d06f97
Erik Lysne <erik.lysne@inline-test.no>
2023-06-12 09:52:46 +0200

    Disabled eslint rule @typescript-eslint/no-empty-interface

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -6,6 +6,7 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": "off"
   }
